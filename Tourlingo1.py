85b792ad7fe144f442ef47ae8829acb1f8bb9ac9
Pnayiturik <p.nayiturik@alustudent.com>
2023-11-24 17:04:18 +0200

    updating Tourlingo

--- Tourlingo1.py
+++ Tourlingo1.py
@@ -4,3 +4,7 @@ class LanguageTranslatorAPP:
     def __init__(self):
         self.translator= Translator()
         self.user_progress={'translations_completed': '0'}
+    #add def for display menu
+    def translate_text(self):
+        text= input("Enter the text you wish to translate:")
+        source_lang=input("Enter the source language(e.g, en for English):")
