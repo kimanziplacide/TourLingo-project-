ea91d6841cb1523fa901b110223a46fb15bf9a7b
Pnayiturik <p.nayiturik@alustudent.com>
2023-11-24 17:10:02 +0200

    updating it twice

--- Tourlingo1.py
+++ Tourlingo1.py
@@ -9,4 +9,4 @@ class LanguageTranslatorAPP:
         text= input("Enter the text you wish to translate:")
         source_lang=input("Enter the source language(e.g, en for English):")
         target_lang=input("Enter the target language(e.g, es for spanish):")
-        
+        translation=self.translator.translate(text, src=source_lang, dest=target_lang)
