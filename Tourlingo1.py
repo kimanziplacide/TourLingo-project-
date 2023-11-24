6f6f673c9963a7825fe734d8acfebfbf243c154a
kimanziplacide <imanzikplacide603@gmail.com>
2023-11-24 17:15:25 +0200

    adding print message

--- Tourlingo1.py
+++ Tourlingo1.py
@@ -4,9 +4,20 @@ class LanguageTranslatorAPP:
     def __init__(self):
         self.translator= Translator()
         self.user_progress={'translations_completed': '0'}
-    #add def for display menu
+        
+    def display_menu(self):
+        print("Welcome to TOURLINGO here is the MENU")
+        print("1. Translate Text")
+        print("2. Language Quiz")
+        print("3. User Progress")
+        print("4. Exit")
+
     def translate_text(self):
         text= input("Enter the text you wish to translate:")
-        source_lang=input("Enter the source language(e.g, en for English):")
-        target_lang=input("Enter the target language(e.g, es for spanish):")
-        translation=self.translator.translate(text, src=source_lang, dest=target_lang)
+        source_lang = input("Enter the source language (e.g., en for English): ")
+        target_lang = input("Enter the target language (e.g., es for Spanish): ")
+        translation= self.translator.translate(text, src=source_lang, dest=target_lang)
+
+        print(f"\nOriginal Text ({source_lang}): {text}")
+        print(f"Translation ({target_lang}): {translation.text}\n")
+
